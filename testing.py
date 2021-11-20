ba229f9d6ce71dc6d732323652eeb4bfbbfa0013
root <root@ip-172-31-6-133.us-east-2.compute.internal>
2021-11-20 14:02:25 +0000

    first commit

---
+++ testing.py
@@ -0,0 +1,22 @@
+# if elif and else
+
+running
+
+route = input("Enter the route: ")
+
+if route.casefold() == "sarjapur":
+    print("pls take green line metro")
+elif route.casefold() == "marathalli":
+    print('pls take Yellow line metro')
+elif route.casefold() == "airport":
+    print('pls take Red line metro')
+else:
+    print("please enter one of these as routes 'sarjapur' or 'marathalli' or 'airport'")
+   
+# gmail, GMAIL, GMail, Gmail, gMAIL --> gmail.com
+
+
+
+
+
+
